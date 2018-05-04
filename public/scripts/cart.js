1bd810ab494e28460cd5aefcf6d66491c77b1349
Nina Yujiri <ninayujiri@gmail.com>
2018-05-04 16:07:37 +0000

    edited cart functionality

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,10 +1,12 @@
 $( document ).ready(function(){
   var foods = new Object();
+
   /* start processing on click */
  $(document).on("click",".add-foods",function(){
    var data      = $(this).data();          // defined data in button
    var quantity  = $(data.id).val();        // selected quqntity
-   var sub_total = Math.round(data.price*quantity);  // price x quantity
+   var sub_total = Math.round(data.price * quantity);  // price x quantity
+
     // define obj
     console.log('PRICE: ',data.price);
     console.log('QTY: ',quantity);
@@ -35,25 +37,24 @@ $( document ).ready(function(){
 
     // It's an obj and cannot POST - change it to JSON here.
     var data = JSON.stringify(foods);
-    $("#foods_list").html(html); //add html to the top of the cart.
-    $("#cart_detail").show();    //open the cart.
-    $("#data").val(data);        //set POST[data] with cart contents
-
+      $("#foods_list").html(html); //add html to the top of the cart.
+      $("#cart_detail").show();    //open the cart.
+      $("#data").val(data);        //set POST[data] with cart contents
   }
     /* POST the form */
-    $(document).on('click',"#go_cart a",function(){
+    $(document).on('click',"#go_cart",function(){
       document.form.submit();
     });
 
   /* Close the cart */
-    $(document).on('click',"#close_cart span",function(){
-      $("#cart_detail").hide();
-    });
+    // $(document).on('click',"#close_cart span",function(){
+    //   $("#cart_detail").hide();
+    // });
     // http://studio-key.com/1792.html
 });
 
 function comma(num) {
-      return num.toString().replace( /([0-9]+?)(?=(?:[0-9]{3})+$)/g , '$1,' );
+  return num.toString().replace( /([0-9]+?)(?=(?:[0-9]{3})+$)/g , '$1,' );
 }
 
 
--- public/scripts/dishes.js
+++ public/scripts/dishes.js
@@ -1,8 +1,8 @@
-$(function(){
-  console.log("clicked!");
-  var foods = new Object();
-  /* start processing on click */
- $('.card-body button').on('click',".add-foods",function(){
+// $(function(){
+//   console.log("clicked!");
+ //  var foods = new Object();
+ //  /* start processing on click */
+ // $('.card-body button').on('click',".add-foods",function(){
  //   var data      = $(this).data();          //defined data in button
  //   var quantity  = $(data.id).val();        //selected quqntity
  //   var sub_total = data.price * quantity;  //price x quantity
@@ -16,44 +16,44 @@ $(function(){
  //   };
  //   cart_open();
 
-  });
+ //  });
 
-  // /* create html for the contents of the cart */
-  // var cart_open = function(){
-  //   $("#food_list").html('');
+ //  /* create html for the contents of the cart */
+ //  var cart_open = function(){
+ //    $("#food_list").html('');
 
-  //   var html = '<ul>';
-  //   var key;
-  //   var total = 0;
-  //   for (key in foods){
-  //     html  += '<li>'+foods[key].name+' QTY:'+foods[key].quantity+' &nbsp;&nbsp;'+comma( foods[key].sub_total )+'Total(USD)</li>';
-  //     total += foods[key].sub_total;
-  //   }
-  //   html += '</ul>';
-  //   html += '<div id="total">Total: (USD)'+comma( total )+'</div>';
+ //    var html = '<ul>';
+ //    var key;
+ //    var total = 0;
+ //    for (key in foods){
+ //      html  += '<li>'+foods[key].name+' QTY:'+foods[key].quantity+' &nbsp;&nbsp;'+comma( foods[key].sub_total )+'Total(USD)</li>';
+ //      total += foods[key].sub_total;
+ //    }
+ //    html += '</ul>';
+ //    html += '<div id="total">Total: (USD)'+comma( total )+'</div>';
 
-  //   // It's an obj and cannot POST - change it to JSON here.
-  //   var data = JSON.stringify(foods);
+ //    // It's an obj and cannot POST - change it to JSON here.
+ //    var data = JSON.stringify(foods);
 
-  //   $("#foods_list").html(html); //add html into top the cart.
-  //   $("#cart_detail").show();    //open the cart.
-  //   $("#data").val(data);        //set POST[data] with cart contents
+ //    $("#foods_list").html(html); //add html into top the cart.
+ //    $("#cart_detail").show();    //open the cart.
+ //    $("#data").val(data);        //set POST[data] with cart contents
 
-  // }
-  //   /* POST the form */
-  //   $(document).on('click',"#go_cart a",function(){
-  //     document.form.submit();
-  //   });
+ //  }
+ //    /* POST the form */
+ //    $(document).on('click',"#go_cart a",function(){
+ //      document.form.submit();
+ //    });
 
-  // /* Close the cart */
-  //   $(document).on('click',"#close_cart span",function(){
-  //     $("#cart_detail").hide();
-  //   });
+ //  /* Close the cart */
+ //    $(document).on('click',"#close_cart span",function(){
+ //      $("#cart_detail").hide();
+ //    });
 
-});
+// });
 
-function comma(num) {
-      return num.toString().replace( /([0-9]+?)(?=(?:[0-9]{3})+$)/g , '$1,' );
-}
+// function comma(num) {
+//       return num.toString().replace( /([0-9]+?)(?=(?:[0-9]{3})+$)/g , '$1,' );
+// }
 
 
