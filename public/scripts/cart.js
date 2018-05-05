4df8d7ad438d5140c5f1c4031a1b5682e637073a
Yasu <yassantune@gmail.com>
2018-05-05 00:48:20 +0000

    added func to send data to db, incomplete

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,33 +1,44 @@
 $( document ).ready(function(){
-  var foods = new Object();
+  let foods = new Object();
 
   /* start processing on click */
  $(document).on("click",".add-foods",function(){
-   var data      = $(this).data();          // defined data in button
-   var quantity  = $(data.id).val();        // selected quqntity
-   var sub_total = Math.round(data.price * quantity);  // price x quantity
+   const data      = $(this).data();          // defined data in button
+   const quantity  = $(data.id).val();        // selected quqntity
+   const sub_total = Math.round(data.price * quantity);  // price x quantity
 
-    // define obj
-    console.log('PRICE: ',data.price);
-    console.log('QTY: ',quantity);
-    console.log('SUB TTL: ', (data.price * quantity));
    foods[data.number] = new Object();  // reset same menu
    foods[data.number] = {
       'name':data.name,
       'price':data.price,
       'quantity': quantity,
-      'sub_total':sub_total
+      'sub_total':sub_total,
+      'id':data.number
    };
    cart_open();
   });
 
+  function getCartInfo(menu, user, phone){
+    let output = {};
+    let orderInfo = [];
+    for (let key in menu) {
+      orderInfo.push({'dish_id': menu[key].id,'quantity': menu[key].quantity}) ;
+    }
+    output = {
+      'userName': user,
+      'phone_number': phone,
+      'dishes': orderInfo
+    }
+    return output;
+  }
+
   /* create html for the contents of the cart */
-  var cart_open = function(){
+    const cart_open = function(){
     $("#food_list").html('');
 
-    var html = '<ul class="list-group mb-3">';
-    var key;
-    var total = 0;
+    let html = '<ul class="list-group mb-3">';
+    let key;
+    let total = 0;
     for (key in foods){
       html  += '<li class="list-group-item d-flex justify-content-between lh-condensed"><div><h6 class="my-0">'+foods[key].name+' <br /><small class="text-muted">Quantity:'+foods[key].quantity+'</small></div><span class="text-muted"> $'+comma( foods[key].sub_total )+'</span></li>';
       total += foods[key].sub_total;
@@ -35,26 +46,52 @@ $( document ).ready(function(){
     html += '<li class="list-group-item d-flex justify-content-between"><span>Total (USD)</span><strong>$'+comma( total )+'</strong></li>';
     html += '</ul>';
 
-    // It's an obj and cannot POST - change it to JSON here.
-    var data = JSON.stringify(foods);
+    const data = JSON.stringify(foods);
       $("#foods_list").html(html); //add html to the top of the cart.
       $("#cart_detail").show();    //open the cart.
-      $("#data").val(data);        //set POST[data] with cart contents
-  }
-    /* POST the form */
-    $(document).on('click',"#go_cart",function() {
-      $('#orderForm').submit();
+      //$("#data").val(data);        //set POST[data] with cart contents
+
+    $(document).on('click','#go_cart_btn',function(e){
+      e.preventDefault();
+      const user = $('#user').val();
+      const phoneNum = $('#phoneNum').val();
+      if(!user || !phoneNum){
+        $('.error').fadeIn();
+        $('#user, #phoneNum').on('keyup', function(){
+          $('.error').fadeOut();
+        });
+      } else {
+        const output = getCartInfo(foods, user, phoneNum);
+        //console.log(output);
+
+        $.ajax({
+          url: '/orders',
+          method: 'PUT',
+          data: output,
+          success: function(data, status, jqXHR){
+            if (status !== 'success') {
+              $errorMsg = 'There was an error. Please try again.';
+              throw 'Request was not a success';
+            } else {
+              $('#go_cart').fadeout();
+            }
+          }
+        })
+      }
     });
 
-  /* Close the cart */
-    // $(document).on('click',"#close_cart span",function(){
-    //   $("#cart_detail").hide();
+  }
+    // /* POST the form */
+    // $(document).on('click','#go_cart',function() {
+    //   console.log(renderCartInfo);
+    //   //console.log(data);
+    //   //$('#orderForm').submit();
     // });
-    // http://studio-key.com/1792.html
 });
 
 function comma(num) {
   return num.toString().replace( /([0-9]+?)(?=(?:[0-9]{3})+$)/g , '$1,' );
 }
+
 
 
