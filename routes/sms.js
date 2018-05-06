6a7e72086d9ad955c001478ff5c7a11c213a3470
kaanyalti <ozankaanyalti@gmail.com>
2018-05-06 15:59:02 +0000

    minor layout changes

--- routes/sms.js
+++ routes/sms.js
@@ -8,14 +8,8 @@ module.exports = (twilio, smsFunctions) => {
   const MessagingResponse = twilio.twiml.MessagingResponse;
 
   router.post("/", (req,res) => {
-
     const responseSMS = req.body.Body;
     smsFunctions.smsCustomer(false, twilioInfo.personal_phone, responseSMS);
-    // const twiml = new MessagingResponse();
-
-    // twiml.message("Your msesage is received")
-    // res.writeHead(200, {'Content-Type': 'text/xml'});
-    // res.end(twiml.toString());
   });
  return router;
 }
--- twilioFunctions/twilioFunctions.js
+++ twilioFunctions/twilioFunctions.js
@@ -16,25 +16,25 @@ module.exports = (twilioClient) => {
       smsCustomer: (isReady, clientPhone, message) => {
                     if(!isReady && message){
                       twilioClient.messages.create({
-                          body: message,
-                          to:clientPhone,
-                          from: twilioInfo.twilioNumber
-                          })
-                          .then((message) => console.log(message.sid));
+                        body: message,
+                        to:clientPhone,
+                        from: twilioInfo.twilioNumber
+                        })
+                        .then((message) => console.log(message.sid));
                     } else if (!isReady){
                       twilioClient.messages.create({
-                          body: "Your order is received by the restaurant.",
-                          to:clientPhone,
-                          from: twilioInfo.twilioNumber
-                          })
-                          .then((message) => console.log(message.sid));
+                        body: "Your order is received by the restaurant.",
+                        to:clientPhone,
+                        from: twilioInfo.twilioNumber
+                        })
+                        .then((message) => console.log(message.sid));
                     } else {
                       twilioClient.messages.create({
-                      body: "Your food is ready for pickup",
-                      to:clientPhone,
-                      from: twilioInfo.twilioNumber
-                      })
-                      .then((message) => console.log(message.sid));
+                        body: "Your food is ready for pickup",
+                        to:clientPhone,
+                        from: twilioInfo.twilioNumber
+                        })
+                        .then((message) => console.log(message.sid));
                     }
 
     }
