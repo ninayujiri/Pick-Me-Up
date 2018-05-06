977c4aaac5d6e16e070d0c71e39a14b2b09ede4d
Nina Yujiri <ninayujiri@gmail.com>
2018-05-06 16:03:54 +0000

    small edits

--- public/scripts/restaurantOrdersTable.js
+++ public/scripts/restaurantOrdersTable.js
@@ -1,13 +1,12 @@
-// let orderData;
 
 // Generate Orders Table
-function generateTableRow(orderObj){
+function generateTableRow(orderObj) {
   // console.log(orderObj);
   const $row          = $("<tr>").addClass("table-white");
   const $orderNumber  = $("<td>").append($("<a>").text(orderObj.id).attr("href",`http://localhost:8080/restaurants/1/orders/${orderObj.id}`));
   const $customerName = $("<td>").text(orderObj.name);
   const $phoneNumber  = $("<td>").text(orderObj.phone_number);
-  const $timePlaced   = $("<td>").text(moment(orderObj.created_at).format('MM/DD, h:mm a'));
+  const $timePlaced   = $("<td>").text(moment(orderObj.created_at).format('MM/DD, h:mm A'));
   const $button       = $("<button>").addClass("btn btn-secondary centered").text("Send SMS");
 
 
@@ -16,16 +15,15 @@ function generateTableRow(orderObj){
 };
 
 
-function renderRows(orderData){
-  $.each(orderData, function(index, value){
+function renderRows(orderData) {
+  $.each(orderData, function(index, value) {
     $("tbody").append(generateTableRow(value))
   })
 }
 
 
-$.get("/restaurants/1/orders/fetch", function(res){
+$.get("/restaurants/1/orders/fetch", function(res) {
   orderData = res;
-  // console.log(orderData);
 })
 .done(function(){
   $(function(){
@@ -37,10 +35,9 @@ $.get("/restaurants/1/orders/fetch", function(res){
 })
 
 
-
 $(document).ready(function(){
   $('tbody').on('click', 'button', function (event) {
-    let id = $(this).parent().find(".orderId").text();
+    // let phone_number = $(this).parent().find(".phone_number").text();
     // event.preventDefault();
     //   $.ajax({
     //       method: 'PUT',
@@ -52,18 +49,3 @@ $(document).ready(function(){
     $(this).parent().remove();
   });
 });
-
-
-
-
-// $(()=>{
-//   console.log("works")
-//       monitorEvents(document);
-//   $("tbody").on("click", "button", function(){
-//     console.log("clicked")
-//     $.get("restaurants/1/orders/1")
-//   })
-
-// })
-
-
